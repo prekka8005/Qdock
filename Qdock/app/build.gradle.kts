f2827d2019e12a137724f196f65ce7a8f92056bd
prekka8005 <65862371+prekka8005@users.noreply.github.com>
2026-06-27 09:05:29 +0530

    Add signingConfigs for release APK

--- Qdock/app/build.gradle.kts
+++ Qdock/app/build.gradle.kts
@@ -24,9 +24,19 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
